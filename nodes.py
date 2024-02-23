e9657af4b3c9efd3dfef68051552c8f16dee8653
czcz1024 <czcz1024@gmail.com>
2024-02-23 17:10:01 +0800

    no message

--- nodes.py
+++ nodes.py
@@ -9,16 +9,21 @@ class FaceCompareNode:
                 "image1" : ("IMAGE", {}),
                 "image2" : ("IMAGE", {}),
                 "process": (["all", "first"],),
-             }
+             },
+             "hidden": {
+                "unique_id": "UNIQUE_ID",
+                "extra_pnginfo": "EXTRA_PNGINFO",
+            },
         }
 
     RETURN_TYPES = ("FLOAT",)
     RETURN_NAMES = ("scores",)
     OUTPUT_IS_LIST = (True,)
+    OUTPUT_NODE = True
     FUNCTION = "calculate_score"
     CATEGORY = "faceCompare"
     
-    def calculate_score(self, image1, image2,process):
+    def calculate_score(self, image1, image2,process,unique_id = None, extra_pnginfo=None):
         result=[]
         for img1 in image1:            
             x=255. * img1.cpu().numpy()
@@ -35,7 +40,7 @@ class FaceCompareNode:
                 if(img2_encoding==[]):
                     return (1.0,)
                 img2_encoding=img2_encoding[0]
-                face_distances = face_recognition.face_distance([img1_encoding], img2_encoding)
+                face_distances = face_recognition.face_distance([img1_encoding], img2_encoding)[0]
                 item.append(face_distances)
                 if(process=="first"):
                     break            
@@ -44,5 +49,17 @@ class FaceCompareNode:
                 break            
             else:
                 result.append(item)        
-        print(result)
-        return (result,)
+        print(result)        
+        # print("unique_id:",unique_id)
+        # print("extra_pnginfo:",extra_pnginfo)
+        # if unique_id and extra_pnginfo:
+        #     workflow=extra_pnginfo["workflow"]
+        #     node=list((x for x in workflow["nodes"] if str(x["id"])==unique_id))[0]
+        #     if node:
+        #         print('find node')
+        #         node['widgets_values'].append("aaa")
+        return {"ui": {"text": str(result)}, "result": (result,)}
+
+NODE_CLASS_MAPPINGS = {
+    "FaceCompare": FaceCompareNode,
+}
